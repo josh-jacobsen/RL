e2aff513dab697a3692792cfa76e02a4fc8be271
Josh Jacobsen <joshjacobsen5@gmail.com>
2017-01-03 01:28:39 +1300

    continuation

--- RLTutorial/Program.cs
+++ RLTutorial/Program.cs
@@ -66,7 +66,17 @@ namespace RLTutorial
                         quit = true;
                         break;
                 }
-
+                // Ensure player is still on screen
+                // Ensure player is still on the screen
+                
+                if (playerX < 0)
+                  playerX = 0;
+                if (playerX > screenWidth - 1)
+                  playerX = screenWidth - 1;
+                if (playerY < 0)
+                  playerY = 0;
+                if (playerY > screenHeight - 1)
+                  playerY = screenHeight - 1;
             }
         }
 
